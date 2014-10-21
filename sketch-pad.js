53bcbab1e2316700ba7f8ff9b2443ec00c37bf11
Jeremy A Kansas <ymerejsasnak@gmail.com>
2014-10-21 11:18:00 -0400

    added working color sliders

--- sketch-pad.js
+++ sketch-pad.js
@@ -10,6 +10,9 @@ $(document).ready(function() {
     	}
     }
 
+    var red = 0;
+    var green = 0;
+    var blue = 0;
 
     var isDown = false;
 
@@ -25,16 +28,33 @@ $(document).ready(function() {
     $("#gridcontainer").on("mousemove", "div", function() {
         if (isDown){
         	$(this).addClass("entered");
+            $(this).css("background-color", "rgb(" + red + "," + green +"," + blue + ")");
         }
     });
 
     $("#gridcontainer").on("mousedown", "div", function() {
     	$(this).addClass("entered");
-
+        $(this).css("background-color", "rgb(" + red + "," + green +"," + blue + ")");
     });
 
     $("button").on("click", function() {
+        $(".entered").css("background-color", "#ffffff")
     	$("div").removeClass("entered");
     });
 
+
+    $("#red").on("change", function() {
+        red = $(this).val()
+        $("#currentcolor").css("background-color", "rgb(" + red + "," + green +"," + blue + ")");
+    });
+    
+    $("#green").on("change", function() {
+        green = $(this).val()
+        $("#currentcolor").css("background-color", "rgb(" + red + "," + green +"," + blue + ")");
+    });
+
+    $("#blue").on("change", function() {
+        blue = $(this).val()
+        $("#currentcolor").css("background-color", "rgb(" + red + "," + green +"," + blue + ")");
+    });
 });
