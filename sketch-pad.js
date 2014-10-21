d97a1c0101e9d31e1217d69574f72d34ef3192d1
Jeremy A Kansas <ymerejsasnak@gmail.com>
2014-10-20 22:44:58 -0400

    started adding color selection sliders

--- sketch-pad.js
+++ sketch-pad.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     
     for (i = 0; i < 16; i++) {
     	for(j = 0; j < 16; j++){
-    	    $("#gridcontainer").append($("<div></div>"));
+    	    $("#gridcontainer").append($("<div class='gridbox'></div>"));
     	}
     }
 
