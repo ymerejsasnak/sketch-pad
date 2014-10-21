e93d730f34c7539f4e78331fad05137f0a83c3b4
Jeremy A Kansas <ymerejsasnak@gmail.com>
2014-10-21 13:48:54 -0400

    added flood fill.  some refactoring.

--- sketch-pad.js
+++ sketch-pad.js
@@ -1,5 +1,7 @@
 
-
+getColor = function(red, green, blue) {
+    return "rgb(" + red + "," + green +"," + blue + ")"
+}
 
 
 $(document).ready(function() {
@@ -13,7 +15,7 @@ $(document).ready(function() {
     var red = 0;
     var green = 0;
     var blue = 0;
-
+    
     var isDown = false;
 
     $(document).on("mousedown", function() {
@@ -25,36 +27,37 @@ $(document).ready(function() {
     });
 
 
-    $("#gridcontainer").on("mousemove", "div", function() {
+    $("#gridcontainer").on("mousemove", ".gridbox", function() {
         if (isDown){
-        	$(this).addClass("entered");
-            $(this).css("background-color", "rgb(" + red + "," + green +"," + blue + ")");
+        	$(this).css("background-color", getColor(red, green, blue));
         }
     });
 
-    $("#gridcontainer").on("mousedown", "div", function() {
-    	$(this).addClass("entered");
-        $(this).css("background-color", "rgb(" + red + "," + green +"," + blue + ")");
+    $("#gridcontainer").on("mousedown", ".gridbox", function() {
+    	$(this).css("background-color", getColor(red, green, blue));
     });
 
-    $("button").on("click", function() {
-        $(".entered").css("background-color", "#ffffff")
-    	$("div").removeClass("entered");
+    $("#clear").on("click", function() {
+        $(".gridbox").css("background-color", "white")
+    });
+
+    $("#flood").on("click", function() {
+        $(".gridbox").css("background-color", getColor(red, green, blue));
     });
 
 
     $("#red").on("change", function() {
         red = $(this).val()
-        $("#currentcolor").css("background-color", "rgb(" + red + "," + green +"," + blue + ")");
+        $("#currentcolor").css("background-color", getColor(red, green, blue));
     });
     
     $("#green").on("change", function() {
         green = $(this).val()
-        $("#currentcolor").css("background-color", "rgb(" + red + "," + green +"," + blue + ")");
+        $("#currentcolor").css("background-color", getColor(red, green, blue));
     });
 
     $("#blue").on("change", function() {
         blue = $(this).val()
-        $("#currentcolor").css("background-color", "rgb(" + red + "," + green +"," + blue + ")");
+        $("#currentcolor").css("background-color", getColor(red, green, blue));
     });
 });
